f1848a1fd2f78891d4cfe29e3a639c1e7e6f7230
AdamPillis <adam.pillis@hotmail.com>
2022-02-09 12:38:08 +0000

    Add: more test to test_models.py and test_views.py

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+from .models import Item
+
+
+class TestModels(TestCase):
+    """To test Item Model"""
+
+    def test_done_defaults_to_false(self):
+        """To test done default is set to false"""
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
